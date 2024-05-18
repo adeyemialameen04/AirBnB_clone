3125995bf298d0a7e40b1ab4e3ff5ca51d8873e3
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-05-18 22:29:46 +0100

    basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,31 +3,60 @@
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     """The test class for basemodel."""
 
     def setUp(self):
-        self.storage = FileStorage()
+        self.model1 = BaseModel()
+        self.model2 = BaseModel()
+        self.model1.name = "Model 1"
+        self.model1.num = 1
+        self.model2.name = "Model 2"
+        self.model2.num = 2
 
     def tearDown(self):
-        self.storage.__FileStorage__object = {}
+        del self.model1
+        del self.model2
 
-    def test_pub_attrs(self):
-        model = BaseModel()
-        model.name = "My model"
-        model.number = 15
-        self.assertEqual(model.name, "My model")
-        self.assertEqual(model.number, 15)
+    def test_id(self):
+        self.assertIsNotNone(self.model1.id)
+        self.assertIsInstance(self.model1.id, str)
+        self.assertNotEqual(self.model1.id, self.model2.id)
 
-    def test_datetime(self):
-        """tests for datetime"""
-        pass
+    def test_dateimes(self):
+        now = datetime.now()
+        self.assertIsInstance(self.model1.created_at, datetime)
+        self.assertIsInstance(self.model1.updated_at, datetime)
+        self.assertIsInstance(self.model2.created_at, datetime)
+        self.assertIsInstance(self.model2.updated_at, datetime)
+        self.assertLessEqual(self.model1.created_at, now)
+        self.assertLessEqual(self.model1.updated_at, now)
 
-    def test_datetime(self):
-        """tests for datetime"""
-        pass
+
+    def test_str(self):
+        str_gotten = str(self.model1)
+        str_expected = f"[BaseModel] ({self.model1.id}) {self.model1.__dict__}"
+        self.assertEqual(str_gotten, str_expected)
+
+    def test_save_method(self):
+        prev = self.model1.updated_at
+        self.model1.save()
+        self.assertNotEqual(prev, self.model1.updated_at)
+        self.assertLessEqual(self.model1.created_at, self.model1.updated_at)
+
+    def test_to_dict_method(self):
+        obj_dict = self.model1.to_dict()
+        self.assertIsNotNone(obj_dict["created_at"])
+        self.assertIsInstance(obj_dict, dict)
+        self.assertIsInstance(obj_dict["id"], str)
+        self.assertEqual(obj_dict["__class__"], "BaseModel")
+        self.assertEqual(obj_dict["id"], self.model1.id)
+        self.assertEqual(obj_dict["created_at"], self.model1.created_at.isoformat())
+        self.assertEqual(obj_dict["updated_at"], self.model1.updated_at.isoformat())
+        self.assertEqual(obj_dict["name"], self.model1.name)
+        self.assertEqual(obj_dict["num"], self.model1.num)
 
 
 if __name__ == "__main__":
